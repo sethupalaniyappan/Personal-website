29c8af12b49a1d68e09e372ada79c053be5862e7
Sethu Palaniyappan Subramanian <sethu30palani@gmail.com>
2025-06-02 23:29:40 +0530

    Updated export config to deploy the app in git hub pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/Personal-website',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  assetPrefix: '/Personal-website/', // Set the asset prefix for static files
 };
 
 export default nextConfig;
