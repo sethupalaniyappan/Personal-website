073e53d6048f2370f02050d0128eb1506865fc42
Sethu Palaniyappan Subramanian <sethu30palani@gmail.com>
2025-06-17 14:46:58 +0530

    setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/Personal-website',
+  distDir: 'dist',
+  //basePath: '/Personal-website',
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
-  assetPrefix: '/Personal-website/', // Set the asset prefix for static files
+  //assetPrefix: '/Personal-website/', // Set the asset prefix for static files
 };
 
 export default nextConfig;
