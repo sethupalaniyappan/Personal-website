3934363b35220ff97d9d1720afc22c59b2511b11
Sethu Palaniyappan Subramanian <sethu30palani@gmail.com>
2025-06-25 11:37:43 +0530

    Reverted the distDirectory to default out folder for debugging.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: 'dist',
+  //distDir: 'dist',
   //basePath: '/Personal-website',
   images: {
     unoptimized: true, // Disable image optimization for static export
