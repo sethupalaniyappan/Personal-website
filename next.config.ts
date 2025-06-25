577fa2038f8b670d2fe29076f8973b30c87b52c8
Sethu Palaniyappan Subramanian <sethu30palani@gmail.com>
2025-06-25 20:51:20 +0530

    Add / in the basepath to fix navigation issue.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/Personal-website',
+  basePath: '/Personal-website/',
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
